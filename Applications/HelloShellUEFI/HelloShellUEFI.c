e19d8f86607f57d2fd6747d7d4654ae64719a4ce
manoelrui <manoelrui19@gmail.com>
2018-04-23 22:46:23 -0300

    Improved output message and code style in HelloShellUEFI

--- Applications/HelloShellUEFI/HelloShellUEFI.c
+++ Applications/HelloShellUEFI/HelloShellUEFI.c
@@ -2,23 +2,17 @@
 #include  <Library/UefiLib.h>
 #include  <Library/ShellCEntryLib.h>
 
-/***
-  Print a welcoming message.
 
-  Establishes the main structure of the application.
-
-  @retval  0         The application exited normally.
-  @retval  Other     An error occurred.
-***/
 INTN
 EFIAPI
 ShellAppMain (
-  IN UINTN Argc,
+  IN UINTN  Argc,
   IN CHAR16 **Argv
   )
 {
-  Print(L"Hello there fellow Programmer.\n");
-  Print(L"Welcome to the world of EDK II.\n");
+  Print(L"Hello UEFI.\n");
+  Print(L"We are using the shell entry point.\n");
+  Print(L"This is the world of EDK II.\n");
 
   return(0);
 }
